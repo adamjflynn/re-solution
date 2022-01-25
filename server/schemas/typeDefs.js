785f7fe6f594b96435b892d59dccc990cb05e854
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-25 13:24:55 -0600

    server.js, downloaded back end dep, added schema file, typedefs and resolvers file, and index.js for schema folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+const { User } = require('../models'); //need to verify this route
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        savedWorkouts: [Workouts]
+    }
+    
+    type Workout {
+        workoutId: String
+        name: String
+        targetMuscleGroup: String
+        targetMuscle: String
+        equiptment: String
+    }
+    
+    type Auth {
+        token: ID!
+        User: user
+    }
+    
+    type Query {
+        me: User
+    }
+     input SavedWorkout {
+        workoutId: String
+        name: String
+        targetMuscleGroup: String
+        targetMuscle: String
+        equiptment: String
+        weight: Int
+        repitions: Int
+        sets: Int
+     }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!. password: String!): Auth
+        saveWorkout(input: SavedWorkout!): User
+        removeWorkout(workoutId: String!): User
+    }`
+
+    module.exports = typeDefs;
