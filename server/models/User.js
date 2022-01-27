ab19e9ad5b6fe887a9b105bc7631aba381884e39
Steven Slocum <stevesl23@hotmail.com>
2022-01-27 00:47:07 -0600

    created initial user schema with email, password, and savedworkouts

--- server/models/User.js
+++ server/models/User.js
@@ -3,9 +3,26 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bcrypt = require('bcrypt');
+const Workout = require('./Workout');
+
 
 const userSchema = new Schema({
 
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    savedWorkouts: [Workout.schema]
 });
 
 const User = mongoose.model('User', userSchema)
