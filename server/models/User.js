5e5a4d3737f8b7c04ac85da7e5ba23cfe5467052
Steven Slocum <stevesl23@hotmail.com>
2022-01-27 00:24:21 -0600

    created initial User Model

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
