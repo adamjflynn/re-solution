43aef2d4955653778645383fca475a97807a1501
Steven Slocum <stevesl23@hotmail.com>
2022-01-27 20:25:25 -0600

    created initial workout model

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -2,7 +2,31 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const Exercise = require('./Exercise')
+
 const workoutSchema = new Schema({
+
+exercise: [Exercise.schema],
+
+date: {
+  type: Date,
+  required: true
+}, 
+
+duration: {
+  type: Number,
+  length: 1
+},
+
+reps: {
+    type: Number,
+    length: 1
+},
+
+sets: {
+    type: Number,
+    length: 1
+}
 
 });
 
