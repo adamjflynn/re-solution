25c80987afca47d8d6ba4244bce59a10e9f2c970
Steven Slocum <stevesl23@hotmail.com>
2022-01-28 01:14:08 -0600

    added seed.js logic to drop all info for all tables and set the insert logic for all schemas

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -2,6 +2,33 @@ const db = require('./connection');
 const { User, Exercise, Workout } = require('../models')
 
 db.once('open', async () => {
+    await Exercise.deleteMany();
+
+    const excerises = await Exercise.insertMany([
+        {
+
+        }
+
+    ])
+
+    console.log("Exercises seeded!")
+
+    await Workout.deleteMany();
+
+    const workouts = await Workout.insertMany([
+
+    ])
+
+    console.log("Workouts seeded!")
+
+    await User.deleteMany();
+
+    const users = await User.insertMany([
+
+    ])
+    
+    console.log('Users seeded!')
+
     console.log('Database fully seeded!')
 
     process.exit();
