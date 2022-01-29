56a69f7ce8e5e0bc23024c8556646d59e80f5faa
Steven Slocum <stevesl23@hotmail.com>
2022-01-29 15:06:35 -0600

    updated resolvers, typeDefs, queries, connection.js and seeds.js to get server to run and seed data

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/re-solution)',
-  {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-  }
-);
+  mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/re-solution',
+    {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+    }
+  );
 
 module.exports = mongoose.connection;
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,10 +1,12 @@
+
 const db = require('./connection');
-const { User, Exercise, Workout } = require('../models')
+const { User, Exercise, Workout } = require('../models');
 
 db.once('open', async () => {
     await Exercise.deleteMany();
+    console.log("Deleted All Exercises!")
 
-    const excerises = await Exercise.insertMany([
+    const exercises = await Exercise.insertMany([
         {
             bodyPart:"cardio",
             equipment:"body weight",
@@ -12,29 +14,46 @@ db.once('open', async () => {
             workoutID:"0685",
             name:"run",
             target:"cardiovascular system"     
+        },
+        {
+            bodyPart: "cardio",
+            equipment: "body weight",
+            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3637.gif",
+            workoutID:"3637",
+            name:"wheel run",
+            target:"cardiovascular system",
+        },
+        {
+            bodyPart:"cardio",
+            equipment:"body weight",
+            gifUrl:"http://d205bpvrqc9yn1.cloudfront.net/3223.gif",
+            workoutID:"3223",
+            name:"star jump (male)",
+            target:"cardiovascular system"
         }
 
     ])
 
-    console.log("Exercises seeded!")
+    console.log(exercises);
+    console.log("Exercises seeded!");
 
-    await Workout.deleteMany();
+    // await Workout.deleteMany();
 
-    const workouts = await Workout.insertMany([
+    // const workouts = await Workout.insertMany([
 
-    ])
+    // ])
 
-    console.log("Workouts seeded!")
+    // console.log("Workouts seeded!")
 
-    await User.deleteMany();
+    // await User.deleteMany();
 
-    const users = await User.insertMany([
+    // const users = await User.insertMany([
 
-    ])
+    // ])
     
-    console.log('Users seeded!')
+    // console.log('Users seeded!')
 
-    console.log('Database fully seeded!')
+    // console.log('Database fully seeded!')
 
     process.exit();
 })
