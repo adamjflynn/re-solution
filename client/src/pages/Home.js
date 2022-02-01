3394982dbb49a5a2e499b7764523fd553ffdd0a0
Adam Flynn <adamflynn1@gmail.com>
2022-02-01 16:29:40 -0600

    updated to look more like the mockup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,12 +7,13 @@ return (
 	<main className="flex-row justify-center mb-4">
 			<div className="col-12 col-md-6">
 				<div className="card">
-					<h4 className="card-header">Welcome to (RE)Solution</h4>
+					<h2 className="card-header">Welcome to (RE)Solution</h2>
+          <h5 className="card-header">Your resolution solution</h5>
 					<div className="card-body">
           <button className="btn d-block w-100" type="submit">
           <Link to="/signup">SIGN UP</Link> 
 							</button>
-            <h5 className="card-header">OR</h5>
+            <h4 className="card-header">OR</h4>
 						
 							<button className="btn d-block w-100" type="submit">
 							<Link to="/login">LOGIN</Link>
