7bf0cabc7eb5f0105ca11278e4fe377f3a1b0891
Sandy McCabe <sandytech13@gmail.com>
2022-02-01 11:00:03 -0600

    updated utils, app.js, and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
+// import './App.css';
+// // import Navbar from "./components/Navbar"
+// import Login from "./pages/login"
+
+// function App() {
+//   return (
+//     <div className="App">
+//     <Login />
+    
+//     </div>
+//   );
+// }
+
+import logo from './logo.svg';
 import './App.css';
-// import Navbar from "./components/Navbar"
-import Login from "./pages/login"
 
 function App() {
   return (
     <div className="App">
-    <Login />
-    
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Navbar from './components/Navbar'
-import App from './App'
+// import Navbar from './components/Navbar'
+// import App from './App'
 
-import {
-  BrowserRouter as Router,
-  Routes
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Routes
+// } from 'react-router-dom';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Navbar />
+//       <App />
+//     </Router>
+
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <App />
-    </Router>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
