eb88a382803c0c2590671dce6ecd01f75efc6f7a
Adam Flynn <adamflynn1@gmail.com>
2022-02-01 19:08:06 -0600

    add concurrently

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,14 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-// export const QUERY_USER = gql`
-//   query user($username: String!) {
-//     user(username: $username) {
-//       _id
-//       username
-//       email
-//       }
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      }
       
-//       }
-//     }
-//   }
-// `;
+      }
+    }
+  }
+`;
