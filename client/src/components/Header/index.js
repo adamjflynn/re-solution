304598efff7bb00e4d66399385599d0baef29443
Sandy McCabe <sandytech13@gmail.com>
2022-02-02 20:21:51 -0600

    the header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-        <h1>(RE)Solution</h1>
-        </Link>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+    return (
+      <header className="container-fluid bg-secondary mb-4 py-2">
+        <div className="container flex-row justify-space-around-lg justify-center align-center">
+          <Link to="/">
+          <h1 className="text-tertiary">(RE)</h1><h1 className="text-primary"> Solution</h1>
+          </Link>
+        </div>
+      </header>
+    );
+  };
+  export default Header;
